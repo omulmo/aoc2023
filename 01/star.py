24cdc068d32e442af88f8a307d248cdcce411bf8
Olle Mulmo <omulmo@github.com>
2023-12-08 16:58:36 +0100

    minimized

--- 01/star.py
+++ 01/star.py
@@ -8,8 +8,8 @@ lookup = numbers.split('|')
 value = lambda x: lookup.index(x) if x in lookup else int(x)
 
 def parse(line, pattern):
-    m = regex.findall(pattern, line, overlapped=True)
-    return 10*value(m[0]) + value(m[-1])
+    tokens = regex.findall(pattern, line, overlapped=True)
+    return 10*value(tokens[0]) + value(tokens[-1])
 
 if __name__ == '__main__':
     pattern = '[0-9]' if sys.argv[1] == '1' else '[0-9]|'+numbers
